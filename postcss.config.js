53830128183003ae012a2db5a4e60772566a676e
zkj <whutzkj@163.com>
2018-11-17 17:29:04 +0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "plugins": [
+    require('postcss-flexbugs-fixes'),
+    require("autoprefixer")({
+      flexbox: 'no-2009',
+      stage: 3,
+    }),
+    require("postcss-aspect-ratio-mini"),
+    require("postcss-write-svg")({ utf8: false }),
+    require("postcss-cssnext"),
+    require("postcss-px-to-viewport")({
+      viewportWidth: 750,
+      viewportHeight: 1334,
+      unitPrecision: 3,
+      viewportUnit: 'vw',
+      selectorBlackList: ['.ignore', '.hairlines'],
+      minPixelValue: 1,
+      mediaQuery: false
+    }),
+    require("postcss-viewport-units"),
+    require("cssnano")({
+      preset: "advanced",
+      autoprefixer: false,
+      "postcss-zindex": false
+    })
+
+  ]
+}
